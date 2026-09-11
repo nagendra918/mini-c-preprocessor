1ddf1a37c4b983bde92df1b50bb59b6ff3d6fc5b
Nagendrababu <nagendrababuvadranapu@gmail.com>
2026-09-11 22:34:12 +0530

    Initial project setup

---
+++ src/main.c
@@ -0,0 +1,11 @@
+//main.c 
+#include<stdio.h> 
+int main(int argc,char *argv[]) 
+{     
+    if(argc!=2)     
+    {         
+        printf("Invalid inputs\n");         
+        printf("Usage: mini_cpp <source_file>\n");         
+        return 1;     
+    }     
+}
